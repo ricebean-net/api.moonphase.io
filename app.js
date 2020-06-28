0cb79f8928baf5b60d69bb1002674dea28dd42b3
Stefan Meissner <stefan.meissner@cimpress.com>
2020-06-28 21:11:59 +0200

    add version endpoint

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 var express = require("express");
+var pjson = require('./package.json');
+
 var app = express();
 
 const server = app.listen(4200, () => {
     console.log("Server running on port 4200");
 });
 
-app.get("/url", (req, res, next) => {
+app.get("/v1/test", (req, res, next) => {
     res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
 });
+
+app.get("/version", (req, res, next) => {
+    res.json({
+        "version": pjson.version
+    });
+});
+
